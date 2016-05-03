38624cc2c1f69d75eb9c7800efcdd444765fe427
billfero <billfero@gmail.com>
2016-05-03 13:48:41 +0100

    Update model object to include string paths for media files. Impl asynchronous write to realm to persist note objects from text note fragment.

--- app/src/main/java/com/example/demo/model/Note.java
+++ app/src/main/java/com/example/demo/model/Note.java
@@ -1,14 +1,19 @@
 package com.example.demo.model;
 
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
-public class PhotoNote extends RealmObject{
+public class Note extends RealmObject{
 
+    @PrimaryKey
+    private Long mId;
     private String mTitle;
     private String mDescription;
     private String mImagePath;
+    private String mVideoPath;
+    private String mAudioPath;
 
-    public PhotoNote() {  }
+    public Note() {  }
 
     public String getTitle() {
         return mTitle;
@@ -26,6 +31,14 @@ public class PhotoNote extends RealmObject{
         mDescription = description;
     }
 
+    public Long getId() {
+        return mId;
+    }
+
+    public void setId(Long id) {
+        mId = id;
+    }
+
     public String getImagePath() {
         return mImagePath;
     }
@@ -34,8 +47,25 @@ public class PhotoNote extends RealmObject{
         mImagePath = imagePath;
     }
 
+    public String getVideoPath() {
+        return mVideoPath;
+    }
+
+    public void setVideoPath(String videoPath) {
+        mVideoPath = videoPath;
+    }
+
+    public String getAudioPath() {
+        return mAudioPath;
+    }
+
+    public void setAudioPath(String audioPath) {
+        mAudioPath = audioPath;
+    }
+
     @Override
     public String toString() {
         return getTitle();
     }
+
 }
--- app/src/main/java/com/example/demo/ui/fragment/TextNoteFragment.java
+++ app/src/main/java/com/example/demo/ui/fragment/TextNoteFragment.java
@@ -5,13 +5,25 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.demo.R;
+import com.example.demo.model.Note;
 
+import java.util.Random;
+
+import io.realm.Realm;
+import io.realm.RealmAsyncTask;
 import timber.log.Timber;
 
-public class TextNoteFragment extends BaseFragment{
+public class TextNoteFragment extends BaseFragment implements View.OnClickListener{
+
+    private Random mGenerator = new Random();
+    private Realm mRealm;
+    private RealmAsyncTask mTransaction;
+    private EditText mTitle;
+    private EditText mDescription;
 
     public TextNoteFragment() {}
 
@@ -19,17 +31,71 @@ public class TextNoteFragment extends BaseFragment{
         return new TextNoteFragment();
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRealm = Realm.getDefaultInstance();
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_text_note, container, false);
-        EditText title = (EditText) view.findViewById(R.id.text_note_title);
-        EditText description = (EditText) view.findViewById(R.id.text_note_description);
 
-        String titleString = title.getText().toString();
-        String descriptionString = description.getText().toString();
-        Timber.i("%s, %s", titleString, descriptionString);
+        mTitle = (EditText) view.findViewById(R.id.text_note_title);
+        mDescription = (EditText) view.findViewById(R.id.text_note_description);
+        Button saveButton = (Button) view.findViewById(R.id.save_note);
+        saveButton.setOnClickListener(this);
 
         return view;
     }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mTransaction != null && !mTransaction.isCancelled())
+            mTransaction.cancel();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mRealm != null) {
+            mRealm.close();
+            mRealm = null;
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        final Note textNote = new Note();
+        final Long id = (long) (mGenerator.nextInt(100) + 1);
+        textNote.setId(id);
+        final String strTitle = mTitle.getText() != null ? mTitle.getText().toString() : "";
+        textNote.setTitle(strTitle);
+        final String strDescription = mDescription.getText() != null ? mDescription.getText().toString() : "";
+        textNote.setDescription(strDescription);
+
+        // save the object to the realm asynchronously
+        mTransaction = mRealm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                realm.copyToRealmOrUpdate(textNote);
+            }
+        }, new Realm.Transaction.OnSuccess() {
+            @Override
+            public void onSuccess() {
+                Timber.i("Success! id: %d, title: %s, description: %s", id, strTitle, strDescription);
+            }
+        }, new Realm.Transaction.OnError() {
+            @Override
+            public void onError(Throwable error) {
+                Timber.e("Error writing object to realm, %s", error.getMessage());
+            }
+        });
+    }
+
+
 }
